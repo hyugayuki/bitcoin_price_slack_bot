48e489f1051809a0dff55b7db58348d155c28683
hyugayuki <hyugayuki@hyuugayuukinoMacBook-Pro.local>
2017-11-18 17:35:14 +0900

    Add Bitbank

--- README.md
+++ README.md
@@ -3,11 +3,12 @@
 仮想通貨取引所のオープンAPIを用いて価格情報をリアルタイムで取得し、slackのbotが価格を返答します。
 - coincheck (https://coincheck.com/ja/documents/exchange/api#about)
 - bitFlyer (https://lightning.bitflyer.jp/docs/api)
-- Zaif (https://api.zaif.jp/api/)
+- Zaif (http://techbureau-api-document.readthedocs.io/ja/latest/index.html)
+- Bitbank(https://public.bitbank.cc/)
 
 
 ## どうやるのか
 
 cryptopricebot.slack.com にて、
 @crypto_bot とのダイレクトチャットか、ボットのいるチャンネルで@crypto_botメンションを付け、
-coincheck , zaif , bitflyer が含まれるメッセージで、その文字列に反応し、それぞれの取引所から価格を取得し返答してくれます。
+coincheck , zaif , bitflyer, Bitbank が含まれるメッセージで、その文字列に反応し、それぞれの取引所から価格を取得し返答してくれます。
--- myenv/slack_bot/plugins/my_bot.py
+++ myenv/slack_bot/plugins/my_bot.py
@@ -7,7 +7,7 @@ import json     #josnを扱うためにモジュール
 BF_api_url = "https://api.bitflyer.jp/v1/"
 CC_api_url = "https://coincheck.com/"
 ZF_api_url = "https://api.zaif.jp/api/1/"
-
+BB_api_url = "https://public.bitbank.cc/"
 
 def BF_get_api_call(path):
     method = 'GET'
@@ -33,6 +33,14 @@ def ZF_get_api_call(path):
         })
     return request_data
 
+def BB_get_api_call(path):
+    method = 'GET'
+    request_data = requests.get(
+        BB_api_url  + path
+        ,headers = {
+        })
+    return request_data
+
 @respond_to('bitflyer', re.IGNORECASE)
 def mention_func(message):
     message.reply('取引所bitFlyerの現在のビットコイン価格情報です。')
@@ -50,3 +58,10 @@ def mention_func(message):
     message.reply('取引所Zaifの現在のビットコイン価格情報です。')
     data = ZF_get_api_call('ticker/btc_jpy').json()
     message.reply('最終取引額：' + str(data['last']) + ',　買い注文高値：' + str(data['bid']) + ',　売り注文安値：' + str(data['ask']))
+
+@respond_to('bitbank', re.IGNORECASE)
+def mention_func(message):
+    message.reply('取引所bitbankの現在のビットコイン価格情報です。')
+    data = BB_get_api_call('btc_jpy/ticker').json()
+    data = data['data']
+    message.reply('最終取引額：' + str(data['last']) + ',　買い注文高値：' + str(data['buy']) + ',　売り注文安値：' + str(data['sell']))
